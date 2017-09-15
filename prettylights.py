2ba689d3cf4ee5cbaf8d313b02aba05de8627fbb
Henry Baba-Weiss <henry.babaweiss@gmail.com>
2017-09-14 23:25:25 -0700

    Support disabling lights (process audio only), add timing infrastructure
    To implement the context for the stream callback, introduce a "table"
    base structure for making bags of properties on the fly.
    
    Also tidy up the style, clean up the function names, refactor the stream
    loop to process a queue of lights rather than iterating inside the
    polling loop.

--- prettylights.py
+++ prettylights.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-import array
 import argparse
+import array
+from collections import deque
+import functools
 import random
 import sys
 import time
@@ -14,16 +16,35 @@ SAMPLE_RATE_HZ = 44100
 LIGHT_DIM = 5
 LIGHT_SPOT = 255
 
-def mic_callback(bin_data, frame_count, time_info, status):
+class table(dict):
+    def __init__(self, **kwargs):
+        super(table, self).__init__(**kwargs)
+        self.__dict__ = self
+
+class stream_context(table):
+    prev_time = 0
+
+def stream_got_data(sctx, data, num_frame, time_info, status):
+    current_time = time_info['current_time']
+    if not sctx.prev_time:
+        sctx.prev_time = current_time
+        return (None, pyaudio.paContinue)
+
+    dt = current_time - sctx.prev_time
+    sctx.prev_time = current_time
+
     pwm_data = array.array("h")
-    pwm_data.fromstring(bin_data)
+    pwm_data.fromstring(data)
 
     # TODO well the pwm data seems to be accurate... so i guess we should
     # start applying the mathematics here...
 
-    return (pwm_data, pyaudio.paContinue)
+    return (None, pyaudio.paContinue)
 
-def init_lights(group):
+def init_lights(bridge, group):
+    scene = scenes_by_name["Dope"]  # hehehe
+    bridge.activate_scene(group.group_id, scene.scene_id)
+
     for light in group.lights:
         light.brightness = LIGHT_DIM
 
@@ -35,7 +56,7 @@ def find_group(args, bridge):
     group = matches[0]
     return group
 
-def handle_args(args, bridge, parser):
+def verify_light_config(args, bridge, parser):
     if args.list_groups:
         for g in bridge.groups:
             print "group_id: %d, name: %s" % (g.group_id, g.name)
@@ -53,53 +74,56 @@ def create_parser():
         "Hue programmable lightbulbs."
     )
     opts = parser.add_mutually_exclusive_group()
-    opts.add_argument("-g", "--group", "--id", dest="group_id",
-                    metavar='id', type=int, help="id of group to control")
-    opts.add_argument("-n", "--name", "--group-name", dest="group_name",
-                    metavar='name', help="name of group to control")
+    opts.add_argument("-g", "--group", "--group-id", dest="group_id",
+                      metavar='id', type=int, help="id of group to control")
+    opts.add_argument("-N", "--name", "--group-name", dest="group_name",
+                      metavar='name', help="name of group to control")
     opts.add_argument("-l", "--list", "--list-groups", dest="list_groups",
-                    help="list all available groups and exit",
-                    action="store_true")
+                      help="list all available groups and exit",
+                      action="store_true")
+    opts.add_argument("-n", "--no-lights", "--dry-run", dest="use_lights",
+                      help="process microphone data without affecting lights",
+                      action="store_false")
     return parser
 
 def main():
     parser = create_parser()
     args = parser.parse_args()
-    bridge = phue.Bridge(secrets.HUE_BRIDGE, username=secrets.HUE_TOKEN)
     pactx = pyaudio.PyAudio()
 
-    handle_args(args, bridge, parser)
-    group = find_group(args, bridge)
-    scenes_by_name = {s.name: s for s in bridge.scenes}
+    if args.use_lights:
+        bridge = phue.Bridge(secrets.HUE_BRIDGE, username=secrets.HUE_TOKEN)
+        verify_light_config(args, bridge, parser)
+        group = find_group(args, bridge)
+        init_lights(bridge, group)
 
-    init_lights(group)
-
+    sctx = stream_context()
     stream = pactx.open(
         format=pactx.get_format_from_width(SAMPLE_WIDTH_BYTES, unsigned=False),
-        channels=CHANNELS,
-        rate=SAMPLE_RATE_HZ,
-        input=True,
-        output=True,
-        stream_callback=mic_callback
+        channels=CHANNELS, rate=SAMPLE_RATE_HZ,
+        input=True, output=False,
+        stream_callback=functools.partial(stream_got_data, sctx)
     )
     stream.start_stream()
 
     try:
-        while stream.is_active():
-            scene = scenes_by_name["Dope"]  # hehehe
-            bridge.activate_scene(group.group_id, scene.scene_id)
-
+        if args.use_lights:
             last_light = None
-            for light in group.lights:
+            light_queue = deque(group.lights)
+
+        while stream.is_active():
+            if args.use_lights:
+                light = light_queue.popleft()
+                if not light_queue:
+                    light_queue.extend(group.lights)
+
                 if last_light:
                     last_light.brightness = LIGHT_DIM
+                last_light = light
 
-                light.hue = random.randint(0, 255)
-                light.saturation = random.randint(0, 255)
                 light.brightness = LIGHT_SPOT
 
-                last_light = light
-                time.sleep(0.25)
+            time.sleep(0.25)
     except KeyboardInterrupt:
         pass
 
